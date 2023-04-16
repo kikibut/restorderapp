a639e0af9f6ed7322df0750f17944123499feb1a
Muhammad Zulhilmi <angahamy99@gmail.com>
2023-04-16 13:58:56 +0800

    B032110442
    Distributed App Development

---
+++ restorderapp/src/main/java/my/edu/utem/ftmk/dad/restorderapp/model/OrderTypeRepository.java
@@ -0,0 +1,12 @@
+package my.edu.utem.ftmk.dad.restorderapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import my.edu.utem.ftmk.dad.restorderapp.model.OrderType;
+
+@Repository
+public interface OrderTypeRepository 
+extends JpaRepository<OrderType, Long> {
+
+}
